4c78806fc6eb1f2f8351f05cd78302a0cda923f0
Jing Hu <_US@DESKTOP-PPPRTKB.localdomain>
2017-10-28 20:21:57 +0200

    übung2 von Jing

--- src/hw02_objects/oop_1_bank.py
+++ src/hw02_objects/oop_1_bank.py
@@ -24,14 +24,88 @@ e) Draw a UML diagram representing your Account class. (1 point)
 
 """
 
-
+import re
 class Account:
-    """ Here has to be a documentation string that describes
-    which data objects this class is designed for.
-    You have to remove the pass statement and then write some
-    code for the class. """
-    pass
+    """ Diese Klasse enthält Attribute wie balance, number sowie holder und Methoden
+    wie __init__, withdraw, deposit, print_info, set_holder, __str__ und apply_interest. 
+    Mit der Methode withdraw  wird das Geld vom Konto abgehoben und mit 
+    Methode deposit auf das Konto eingezahlt. 
+    Die Methode print_info gibt den Kontostand zurück und mit set_holder wird 
+    ein anderer Kontoinhaber gesetzt. 
+    Mit apply_interest werden 1.5% Zinsen der Balancehinzugefügt
+    Alle Kontoinformationen werden bei Aufruf als string zurückgegben
+    """
+    
+    # CONSTRUCTOR
+    def __init__(self, num, person):
+        # diese  Methode wird automatisch nach Erstellen des Objekts aufgerufen.
+        # Der Name und die Kontonummer werden gesetzt, die Balance initial auf 0 gesetzt
+        self.balance = 0
+        self.number = num
+        self.holder = person
+        
+    # METHODS
+    def withdraw(self, amount):
+        # mit dieser Methode wird Geld vom Konto abgehoben. Maximal ist ein Kredit von 1000 möglich
+        if self.balance - amount < -1000:
+            amount = self.balance + 1000
+        self.balance -= amount
+        return amount
+    def deposit(self, amount):
+        # mti dieser Methode wird Geld auf das Konto eingezahlt (der Balance hinzugefügt).
+        self.balance += amount
+        return amount
+    def print_info(self):
+        # diese gibt den Kontostand (Balance) zurück
+        print("Balance:", self.balance)
+
+    def set_holder(self, person):
+        # diese Methode setzt einen anderen Kontoinhaber (holder)
+        if (not type(person) == str):
+            raise TypeError
+        if not re.match("\w+( \w+)*", person.strip()):
+            raise ValueError
+        self.holder = person
+    def __str__(self):
+        # Methode, die Informationen bei Aufruf als String zurückgibt
+        res = "*** Account Info ***\n"
+        res += "Account ID:" + str(self.number) + "\n"
+        res += "Holder:" + self.holder + "\n"
+        res += "Balance: " + str(self.balance) + "\n"
+        return res  
+    def apply_interest(self):
+        # diese Methode addiert 1.5% Zinsen zur Balance .
+        self.balance = self.balance + self.balance * 0.015 
 
 if __name__ == "__main__":
     print("Welcome to the Python Bank!")
-
+    
+    annesAcc = Account(1, "Anne")
+    annesAcc.deposit(500)
+    cash = annesAcc.withdraw(2000)
+    print("Oh no, I only got:", cash)
+    annesAcc.print_info()
+    print(annesAcc)
+    
+    stefansAcc = Account(2, "Stefan")
+    stefansAcc.deposit(1000)
+    stefansAcc.print_info()
+    cash = stefansAcc.withdraw(1500)
+    print("Oh no, I only got:", cash)
+    stefansAcc.print_info()
+    stefansAcc.set_holder("Andrea")
+    stefansAcc.print_info()
+    
+    jingsAcc = Account(3, "Jing")
+    jingsAcc.deposit(1000)
+    jingsAcc.print_info()
+    jingsAcc.withdraw(100)
+    jingsAcc.print_info()
+    jingsAcc.apply_interest()
+    print(jingsAcc)
+    
+    willsAcc = Account(4, "Will")
+    willsAcc.deposit(5000)
+    willsAcc.withdraw(1389)
+    willsAcc.set_holder("Jing")
+    print(willsAcc)
--- src/hw02_objects/oop_2_employee.py
+++ src/hw02_objects/oop_2_employee.py
@@ -10,9 +10,62 @@ b) Create a few employee objects and show how you can
 c) Draw a UML class diagram for your Employee class. (1 point)
 """
 
-
 class Employee:
-    pass
+    
+    """ Diese Klasse enthält Attribute wie Name, Geburtzdatung, Geschlecht und Methoden
+    wie __init__, change_name, set_addresse, set_famillienstand, set_gehalt, raise_gehalt und  __str__.
+    Mit der Methode change_name  wird der Name geändert und mit 
+    Methode set_addresse wird die Adresse geändert. 
+    Die Methode set_famillienstand setzt den Famillienstand und bestimmt die Steuerklasse (vereinfacht: 1 vs. 3) 
+    Mit set_gehalt wird das Gehalt gesetzt. 
+    Mit raise_gehalt wird das Gehalt um eine Prozentzahl erhöht.
+    Alle Informationen werden bei Aufruf als string zurückgegben
+    """
+    # constructor
+    def __init__ (self, nam, geb, ges):
+        self.Name = nam
+        self.Geburtzdatung = geb
+        self.Geschlecht = ges
+    # Methods
+    def change_name(self, nam):
+        self.Name = nam
+    def set_addresse(self, add):
+        self.Addresse = add
+    def set_familienstand(self, fam):
+        self.Fam = fam
+        if(self.Fam == "ledig"):
+            self.Steuer = "Klassen 1"
+        elif (self.Fam == "verheiratet"):
+            self.Steuer = "Klassen 3"
+        else:
+            raise ValueError
+    def set_gehalt(self, geh, pos):
+        self.Gehalt = geh
+        self.Position = pos
+    def raise_gehalt(self, pozent):
+        self.Gehalt = self.Gehalt + self.Gehalt * pozent/100
+    def __str__ (self):
+        res = "*** Employee Information ***\n"
+        res += "Name: " + str(self.Name) + "\n"
+        res += "Geschlecht: " + str(self.Geschlecht) + "\n"
+        res += "Address: " + str(self.Addresse) + "\n"
+        res += "Geburtzdatung: " + str(self.Geburtzdatung) + "\n"
+        res += "Familienstand: " + str(self.Fam) + "\n"
+        res += "Steuerklassen: " + str(self.Steuer) + "\n"
+        res += "Gehalt: " + str(self.Gehalt) + "\n"
+        res += "Position: " + str(self.Position) + "\n"
+        return res
 
 if __name__ == "__main__":
-    print("Employee application")
+    print("Employee application" )
+    
+    empAnne = Employee("Anne", "01.06.1997", "Frau")
+    empAnne.change_name("Hanna")
+    empAnne.set_addresse("Werner-Heisenberg-Allee 25, 80939 München")
+    empAnne.set_familienstand("ledig")
+    empAnne.set_gehalt(5000, "Tutor")
+    empAnne.raise_gehalt(3)
+    print(empAnne)
+    
+    
+    
