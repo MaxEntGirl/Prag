2ba2b5c890550430504c6f0394811f7911a59a2b
hujin <hujin@cip.ifi.lmu.de>
2017-11-11 15:46:12 +0100

    Homwork 03 von Jing Hu

--- src/hw03_documents/document.py
+++ src/hw03_documents/document.py
@@ -1,29 +1,44 @@
+from nltk import FreqDist, word_tokenize
 def normalized_tokens(text):
     """ This takes a string and returns lower-case tokens, using nltk for tokenization. """
-    pass # TODO: return list with lower-case tokens.
+    # return list with lower-case tokens.
+    text = text.lower()
+    return word_tokenize(text)
 
 class TextDocument:
     def __init__(self, text, id=None):
         """ This creates a from a string and an identifier. """
         self.text = text
-        self.word_to_count = None # TODO: Create dictionary from words to counts.
+        self.word_to_count = FreqDist(normalized_tokens(text)) #Create dictionary from words to counts.
         self.id = id
 
     @classmethod
     def from_file(cls, filename):
         """ This creates a TextDokument by reading a file. """
-        text = "" # TODO: read text from filename
+        # read text from filename
+        file = open(filename)
+        text = file.read()
+        file.close()
         return cls(text, filename)
 
     def __str__(self):
         """ This returns a short string representation, which is at most 25 characters long.
         If the original text is longer than 25 characters, the last 3 characters of the short string should be '...'.
         """
-        pass # TODO: Implement correct return statement.
+        # Implement correct return statement.
+        if len(self.text) <= 24:
+            return self.text[0:24]
+        else :
+            return self.text[0:22] + '...'
 
     def word_overlap(self, other_doc):
         """ This returns the number of words that occur in both of the documents (self and other_doc) at the same time.
         Every word should be considered only once, irrespective of how often it occurs in either document (i.e. we
         consider word *types*).
         """
-        pass # TODO: Implement correct return statement.
+         # Implement correct return statement.
+        count = 0
+        for key in self.word_to_count:
+            if key in other_doc.word_to_count:
+                count += 1
+        return count
