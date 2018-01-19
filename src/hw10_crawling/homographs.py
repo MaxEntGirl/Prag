c3686a65093b328e44b6a1996f913ec225756aa0
hujin <hujin@cip.ifi.lmu.de>
2018-01-19 23:23:58 +0100

    Homework 10 von Jing Hu

--- src/hw10_crawling/homographs.py
+++ src/hw10_crawling/homographs.py
@@ -4,24 +4,36 @@ from collections import defaultdict
 
 def fill_dict(tagged_text):
     #TODO store mapping between words and all the possible POS of this word, use defaultdict
-    pass
+
+    d = defaultdict(set)
+    for word, pos in tagged_text:
+        d[word].add(pos)
+    return d
 
 def filter_dict(word_dict_h):
+    # TODO delete entry, if not a homograph
+
     for word in dict(word_dict_h).keys():
-        #TODO delete entry, if not a homograph
-        pass
-    
+        if len(word_dict_h[word]) < 2:
+            del word_dict_h[word]
+    return word_dict_h
 
 def filter_dict_nominalization(word_dict):
+    # TODO delete entry, if not a homograph of possible instances of nominalization
+    # or denominalization, restricted to the cases where a verb is used as a noun or the other way round.
+
     for word in dict(word_dict).keys():
-        #TODO delete entry, if not a homograph of possible instances of nominalization
-        #or denominalization, restricted to the cases where a verb is used as a noun or the other way round.
-        pass
-    
+        if 'NN' and 'VB' not in word_dict[word]:
+            del word_dict[word]
+    return word_dict
 
 def find_homographs(tagged_text):
     #TODO return a dictionary to a given tagged text, which hold homographs'''
-    pass
+
+    dic = fill_dict(tagged_text)
+    return filter_dict(dic)
+
+
 
 
 
--- src/hw10_crawling/raw.py
+++ src/hw10_crawling/raw.py
@@ -1,28 +1,62 @@
 import nltk
 import urllib
-import bs4 
+import bs4
+from bs4 import BeautifulSoup
 
 def get_text(url):
     #TODO returns the list of clean paragraphs (no HTML markup) from the given URL
-    pass
-def make_text(raw):   
-    #TODO tokenize the stripped raw text and return an NLTK text object 
-    pass 
+
+    list_text = []
+    urlData = urllib.request.urlopen(url)
+    html = urlData.read().decode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
+    for paragraph in soup.find_all('p'):
+        list_text.append(paragraph.get_text())
+    return list_text
+
+def make_text(raw):
+    #TODO tokenize the stripped raw text and return an NLTK text object
+
+    text = nltk.word_tokenize(raw)
+    new_text = nltk.Text(text)
+    return new_text
+
 def stem_porter(tokens):
     #TODO returns list of stemmed tokens (use PorterStemmer)
-    pass
+
+    porter = nltk.PorterStemmer()
+    stemmed = [porter.stem(t) for t in tokens]
+    return stemmed
+
 def stem_lancaster(tokens):
     #TODO returns list of stemmed tokens (use Lancaster Stemmer)
-    pass
+
+    lancaster = nltk.LancasterStemmer()
+    steammed = [lancaster.stem(t) for t in tokens]
+    return steammed
+
 def lower(text):
     #TODO takes as input an NLTK text object "text" and returns list of lower case tokens
-    pass
+
+    list_token = []
+    for token in text:
+        list_token.append(token.lower())
+    return list_token
+
 def lemmatize(tokens):
     #TODO returns list of lemmas (use WordNetLemmatizer)
-    pass
+
+    wnl = nltk.WordNetLemmatizer()
+    lemmatized = [wnl.lemmatize(t) for t in tokens]
+    return lemmatized
+
 def content(tokens):
     #return list of tokens without stopwords
-    pass
+
+    content = [x for x in tokens if x not in nltk.corpus.stopwords.words('english')]
+    return content
+
+
 
     
 
